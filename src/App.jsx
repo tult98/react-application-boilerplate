90083700ad6960fbbec3300a80e18e596ff4e9aa
tult <lethanhtu1551998@gmail.com>
2021-10-16 18:17:47 +0700

    feat: setup eslint + prettier

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,25 @@ const App = () => {
   return (
     <div>
       <h1>React application boilerplate</h1>
-      <p>Hello nhe' </p>
-      <p>Version 2.1</p>
+      <p>This is an application setup from scratch with full of features like</p>
+      <ul>
+        <li>Webpack as module bundler</li>
+        <li>Babel for transpile code so newest Javascript features are available to use</li>
+        <li>Eslint for enforced code style</li>
+        <li>Prettier for auto formatting code</li>
+        <li>
+          Husky for linting code at pre-commit and pre-push. So everyone in team have a consistent code style despite
+          difference setup
+        </li>
+        <p>
+          You can have a look in{' '}
+          <a href="https://tult.notion.site/Setup-a-react-application-from-scratch-0bdef4016c9f4fd9a3b0461e4d2d63d3">
+            here
+          </a>{' '}
+          where I try my best to explain why we need these tools, dependencies and the instructation to set things up
+          step by step.
+        </p>
+      </ul>
     </div>
   )
 }
