d5a70996bc0e7026c9f245043b76fb93db60d340
tult <lethanhtu1551998@gmail.com>
2021-10-19 09:48:39 +0700

    feat: assests management with webpack

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -8,9 +8,21 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx|ts|tsx)$/,
+        test: /\.(js|jsx|ts|tsx)$/i,
         exclude: /node_modules/,
         use: ['babel-loader'],
+      },
+      {
+        test: /.\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
     ],
   },
